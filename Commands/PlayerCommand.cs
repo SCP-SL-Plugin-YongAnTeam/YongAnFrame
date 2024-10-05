de6f81a7a15d08804a19dc1f92afe3aa813c7285
永安404 <101850798+YongAn404@users.noreply.github.com>
2024-10-06 01:38:40 +0800

    问题的修复 (#6)
    * add BDNT(Bypass Do Not Track)
    
    * add GetAllProperties
    
    * 修复功能

---
+++ Commands/PlayerCommand.cs
@@ -0,0 +1,45 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YongAnFrame.Players;
+
+namespace YongAnFrame.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class PlayerCommand : ICommand
+    {
+        public string Command => "player";
+
+        public string[] Aliases => ["play", "pl", "pr"];
+
+        public string Description => "用于管理自己的用户";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "NULL";
+            if (arguments.Count < 2)
+            {
+                switch (arguments.Array[1])
+                {
+                    case "BDNT":
+                        if (Player.TryGet(sender, out Player player))
+                        {
+                            FramePlayer fPlayer = FramePlayer.Get(player);
+                            fPlayer.HintManager.Clean();
+                            string[] text = new string[36];
+
+                            fPlayer.ExPlayer.ShowHint($"<size=20>{string.Join("\n", text)}\n\n\n\n\n\n\n\n\n\n\n\n\n\n</size>", 10000f);
+                        }
+                        
+                        return true;
+                }
+                
+            }
+            return false;
+        }
+    }
+}
--- YongAnFrameConfig.cs
+++ YongAnFrameConfig.cs
@@ -6,5 +6,6 @@ namespace YongAnFrame
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; }
+        public string BypassDoNotTrackText { get; set; } = "BDNT(Bypass Do Not Track)协议\r\n根据VSR(Verified Server Rules) 8.11，开启DNT(Do Not Track)的玩家不会进行非服务器安全性的游戏数据收集或保存。\r\n根据VSR 8.11.5，只有签署BDNT的玩家才会对DNT相关的规则不适用。\r\n根据VSR 8.11.5.3，欲签署BDNT的玩家有知晓收集或保存数据内容的权利。\r\n|||如果你看不懂BDNT协议的条例请不要签署|||\r\n1.你将会被收集SteamID用来保存等级和称号数据，这个条例收集的数据是公开展示的，任何人都可以访问！\r\n2.签署玩家依然有请求删除收集或保存数据的权利，请求之后你依然有24小时可以撤销请求(注意！删除数据是不可逆的)！";
     }
 }
