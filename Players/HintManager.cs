3d27c9cae6367b4ea13f01340f327a04bc885564
永安404 <101850798+YongAn404@users.noreply.github.com>
2024-10-07 10:20:16 +0800

    修复错版 (#10)

--- Players/HintManager.cs
+++ Players/HintManager.cs
@@ -50,7 +50,6 @@ namespace YongAnFrame.Players
                     if(chatText != null)
                     {
                         text[used] += chatText;
-                        used++;
                         chatText.Duration--;
 
                         if (chatText.Duration <= 0)
@@ -59,6 +58,11 @@ namespace YongAnFrame.Players
                             i--;
                         }
                     }
+                    else
+                    {
+                        text[used] = string.Empty;
+                    }
+                    used++;
                 }
 
                 foreach (Text data in CustomText)
@@ -80,8 +84,12 @@ namespace YongAnFrame.Players
                             MessageTexts.Remove(messageText);
                             i--;
                         }
-
                     }
+                    else
+                    {
+                        text[used] = string.Empty;
+                    }
+                    used++;
                 }
                 text[used] += "</align>";
 
